4f0cc1c12276a1188924822383a9d180b1b16822
ttieman <tiemantanner@gmail.com>
2023-04-07 01:10:40 -0700

    initialize jaws

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,22 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config(); // this reads the .env file and adds the variables to process.env
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // these are the variables from the .env file
-  host: process.env.DB_HOST, // this is the host from the .env file
-  dialect: 'mysql',
-  port: process.env.DB_PORT, // this is the port from the .env file
-  port: 3306,
-  define: {
-    timestamps: false, // this is to disable the default timestamp fields
-    freezeTableName: true, // this is to disable the plural table names
-    underscored: true // this is to use snake_case instead of camelCase
-  }
-});
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // these are the variables from the .env file
+    host: process.env.DB_HOST, // this is the host from the .env file
+    dialect: 'mysql',
+    port: process.env.DB_PORT, // this is the port from the .env file
+    port: 3306,
+    define: {
+      timestamps: false, // this is to disable the default timestamp fields
+      freezeTableName: true, // this is to disable the plural table names
+      underscored: true // this is to use snake_case instead of camelCase
+    }
+
+  })
+};
 
 module.exports = sequelize;
