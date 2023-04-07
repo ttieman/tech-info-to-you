f9d48878538d6290d0a394453ad54270630b4778
ttieman <tiemantanner@gmail.com>
2023-04-07 01:31:13 -0700

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const Handlebars = require('handlebars');
+
 
 const helpers = require('./utils/auth');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
