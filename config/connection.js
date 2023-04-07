02e5fbff8745fdfd172eddcd5513e71361fe8b03
ttieman <tiemantanner@gmail.com>
2023-04-07 01:14:21 -0700

    connect to jaws

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config(); // this reads the .env file and adds the variables to process.env
-
+let sequelize;
 if (process.env.JAWSDB_URL) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else {
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // these are the variables from the .env file
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // these are the variables from the .env file
     host: process.env.DB_HOST, // this is the host from the .env file
     dialect: 'mysql',
     port: process.env.DB_PORT, // this is the port from the .env file
